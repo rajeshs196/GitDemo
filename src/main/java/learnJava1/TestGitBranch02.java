baa4582b4ef1396ff81f986c9ad0c6316677b2c7
rajeshturbo@outlook.com <rajeshturbo@outlook.com>
2026-01-06 16:37:03 +0530

    devlop changes

---
+++ src/main/java/learnJava1/TestGitBranch02.java
@@ -0,0 +1,14 @@
+package learnJava1;
+
+public class TestGitBranch02
+{
+	public void getAA() 
+	{
+		System.out.println("new mad max branch");
+	}
+	
+	public void getNewMeme() 
+	{
+		System.out.println("new john wick branch");
+	}
+}
